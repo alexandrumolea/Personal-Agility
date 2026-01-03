ee7c6acc882e4e2283799f58feb67a01b3d77919
Alexandru Molea <alexandru.molea@bemore.ro>
2026-01-03 20:59:13 +0700

    Initial Commit

---
+++ Projects Manager/Projects_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Projects_ManagerApp.swift
+//  Projects Manager
+//
+//  Created by Alexandru Molea on 03.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct Projects_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
